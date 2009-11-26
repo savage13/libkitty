9dda6e070869cf51815b3e2e494f3700350c0fd9
Brian Savage <savage@uri.edu>
2009-11-26 11:26:54 -0500

    cleaned up tests

--- t/strtol_test.c
+++ t/strtol_test.c
@@ -42,17 +42,17 @@ main(int argc, char *argv[]) {
     while(scanf("%s %ld %ld\n", s, &d, &e) == 3) {
       dout = cint(s, &nerr);
       if(e != nerr) {
-	fprintf(stderr, "int: %10ld <=> %ld err: %4ld <=> %4ld\n", 
-		d, dout, e, nerr);
-	flag = 1;
+        fprintf(stderr, "int: %10ld <=> %ld err: %4ld <=> %4ld\n", 
+                d, dout, e, nerr);
+        flag = 1;
       } else if(e == 0 && d != dout) {
-	fprintf(stderr, "int: %10ld <=> %ld err: %4ld <=> %4ld\n", 
-		d, dout, e, nerr);
-	flag = 1;
+        fprintf(stderr, "int: %10ld <=> %ld err: %4ld <=> %4ld\n", 
+                d, dout, e, nerr);
+        flag = 1;
       } else if(e != 0 && d != dout) {
-	fprintf(stderr, "int: %10ld <=> %ld err: %4ld <=> %4ld\n", 
-		d, dout, e, nerr);
-	flag = 1;
+        fprintf(stderr, "int: %10ld <=> %ld err: %4ld <=> %4ld\n", 
+                d, dout, e, nerr);
+        flag = 1;
       }
     }
   } 
@@ -60,17 +60,17 @@ main(int argc, char *argv[]) {
     while(scanf("%s %lf %ld\n", s, &f, &e) == 3) {
       fout = cdouble(s, &nerr);
       if(e != nerr) {
-	fprintf(stderr, "flt: %14.6e <=> %14.6e err: %4ld <=> %4ld\n", 
-		f, fout, e, nerr);
-	flag = 1;
+        fprintf(stderr, "flt: %14.6e <=> %14.6e err: %4ld <=> %4ld\n", 
+                f, fout, e, nerr);
+        flag = 1;
       } else if(e == 0 && f != fout) {
-	fprintf(stderr, "flt: %14.6e <=> %14.6e err: %4ld <=> %4ld\n", 
-		f, fout, e, nerr);
-	flag = 1;
+        fprintf(stderr, "flt: %14.6e <=> %14.6e err: %4ld <=> %4ld\n", 
+                f, fout, e, nerr);
+        flag = 1;
       } else if(e != 0 && f != fout) {
-	fprintf(stderr, "flt: %14.6e <=> %14.6e err: %4ld <=> %4ld\n", 
-		f, fout, e, nerr);
-	flag = 1;
+        fprintf(stderr, "flt: %14.6e <=> %14.6e err: %4ld <=> %4ld\n", 
+                f, fout, e, nerr);
+        flag = 1;
       }
     }
   } 
